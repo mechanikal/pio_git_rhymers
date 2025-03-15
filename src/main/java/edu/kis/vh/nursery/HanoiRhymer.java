227370f984e0150798973d76924db930f2714b12
251160 <251160@edu.p.lodz.pl>
2025-03-15 10:33:06 +0100

    2.1 poprawa wcięć w pliku HanoiRhymer (przesuniecie wcięcia w funkcji else)

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -11,7 +11,7 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
                 totalRejected++;
-            else
-                super.countIn(in);
+        else
+            super.countIn(in);
     }
 }
