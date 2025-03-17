bde9d6faf19bb11ec45b323d6374852fb83f7246
251197 <251197@helium.com>
2025-03-17 11:38:06 +0100

    6. Dodanie stałych

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,7 +2,9 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int SIZE = 12;
+    public static final int ERR = -1;
+    private int[] numbers = new int[SIZE];
 
     public int total = -1;
 
@@ -12,22 +14,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == ERR;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == SIZE - 1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERR;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERR;
         return numbers[total--];
     }
 
