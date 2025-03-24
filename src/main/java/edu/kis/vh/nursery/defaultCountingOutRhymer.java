df5268db70653625b02b709c216e8f0e433a0eee
251160 <251160@edu.p.lodz.pl>
2025-03-24 11:39:28 +0100

    9. Ograniczono widoczność pól i atrybutów

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,23 +2,23 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int SIZE = 12;
-    public static final int METHOD_ERR = -1;
-    public static final int ARR_EMPTY = -1;
+    protected static final int SIZE = 12;
+    private static final int METHOD_ERR = -1;
+    private static final int ARR_EMPTY = -1;
     private final int[] numbers = new int[SIZE];
 
-    public int total = ARR_EMPTY;
+    protected int total = ARR_EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
-    public boolean callCheck() {
+    protected boolean callCheck() {
         return total == ARR_EMPTY;
     }
 
-    public boolean isFull() {
+    protected boolean isFull() {
         return total == SIZE - 1;
     }
 
